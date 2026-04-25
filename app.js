b0914f3ea1c1f3856b4f20fd488e7df88f026a9a
Ruturaj-007 <pruturaj3003@gmail.com>
2026-04-25 12:31:44 +0530

    feat: configure dotenv for environment variables and update app structure

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express'
+import { PORT } from './config/env.js'
+import { config } from 'dotenv';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Welcome ')
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => {
+    console.log(`Subscription tracker is running on http://localhost:${PORT}`)
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+export default app;
---
+++ config/env.js
@@ -0,0 +1,4 @@
+import { config } from 'dotenv';
+config({path : `.env.${process.env.NODE_ENV || 'development'}.local`});
+export const PORT = process.env.PORT || 5500;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
