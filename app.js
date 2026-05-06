e7ee5c0a04d211ca5d8fef6d80ef7ce2e4107af0
Ruturaj-007 <pruturaj3003@gmail.com>
2026-05-06 19:05:11 +0530

    feat: add error handling middleware & included cookie parser in app str

--- app.js
+++ app.js
@@ -5,15 +5,25 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';   
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middlewares.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended : false
+}));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
 
+app.use(errorMiddleware);
+
 app.get('/', (req, res) => {
-    res.send('Welcome ')
+    res.send('Welcome to Subscription Tracker API!!')
 })
 
 app.listen(PORT, async () => {
